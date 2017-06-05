73bf15723e033f73d7fc32fa6904392f5447473f
Kate Chervin <kchervin@gmail.com>
2017-06-04 21:18:41 -0700

    add questions release 1

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+user_name = gets.chomp
+
+puts "How old are you?"
+stated_age = gets.chomp.to_i
+
+puts "What year were you born?"
+birthYear = gets.chomp.to_i
+
+puts "do you like garlic? (y/n)"
+input = gets.chomp
+
+puts "Do you want insurance? (y/n)"
+input = gets.chomp

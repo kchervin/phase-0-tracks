6efba9cabfe0f9863c08472a7ed74950396c27a1
Kate Chervin <kchervin@gmail.com>
2017-06-08 09:56:41 -0700

    try new way of solving release 0

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -17,3 +17,21 @@ array = [1, 7, 11, 15, 42]
 p method(array, 4)
 p method(array, 1)
 p method(array, 42)
+
+def method(array, int)
+  index = 0
+  new_finding = nil
+array.each do
+    if int == array[index]
+      new_finding = index
+    end
+      index += 1
+  end
+  new_finding
+end
+
+try_array = [1, 7, 11, 15, 42]
+p method(try_array, 4)
+p method(try_array, 1)
+p method(try_array, 42)
+

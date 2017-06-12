c26def87321550fc9b69187eeff94695a3f7d171
Kate Chervin <kchervin@gmail.com>
2017-06-12 13:18:55 -0700

    create 20 new santas with varying attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,6 @@
 class Santa
-attr_reader :age, :ethnicity
-attr_accessor :gender
+attr_reader :ethnicity
+attr_accessor :gender, :age
 
   def initialize(gender, ethnicity)
     puts "Initializing Santa instance..."
@@ -32,25 +32,35 @@ end
 
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-santas = []
+#santas = []
 
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
-end
+#example_genders.length.times do |i|
+ # santas << Santa.new(example_genders[i], example_ethnicities[i])
+#end
 
-p santas
+#p santas
 
-Saint_nick = Santa.new("male", "white")
-Saint_nick.speak
-Saint_nick.eat_milk_and_cookies("Snickerdoodle")
-p Saint_nick.gender= "unicorn latte female"
-p Saint_nick.get_mad_at("Rudolph")
-p Saint_nick.celebrate_birthday
-p Saint_nick.age
-p Saint_nick.ethnicity
-p Saint_nick.gender
+#Saint_nick = Santa.new("male", "white")
+#Saint_nick.speak
+#Saint_nick.eat_milk_and_cookies("Snickerdoodle")
+#p Saint_nick.gender= "unicorn latte female"
+#p Saint_nick.get_mad_at("Rudolph")
+#p Saint_nick.celebrate_birthday
+#p Saint_nick.age
+#p Saint_nick.ethnicity
+#p Saint_nick.gender
 
 
 #attr_reader - readable :name, :location in place of def gender @gender end
 #attr_accessor - readable and write-able
 #attr_writer - write-able  in place of def @gender = new_gender
+
+count = 0
+until count == 20
+  new_santa = Santa.new(example_genders.sample, example_ethnicities.sample)
+  new_santa.age= rand(140)
+  p new_santa.age
+  p new_santa.gender
+  p new_santa.ethnicity
+  count +=1
+end

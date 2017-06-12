986ccc70b944256e51c7077379dde61542a9ebd0
Kate Chervin <kchervin@gmail.com>
2017-06-12 12:42:24 -0700

    release 2

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,13 @@
 class Santa
 
+  def initialize(gender, ethnicity)
+    puts "Initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
+  end
+
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
   end
@@ -8,13 +16,50 @@ class Santa
     puts "that was a good #{cookie}!"
   end
 
-  def intitialize
-    puts "intitializing Santa instance..."
+  def celebrate_birthday
+    @age += 1
+  end
+
+  def get_mad_at(name)
+      @reindeer_ranking.delete(name)
+      @reindeer_ranking << name
+  end
+
+  def gender=(new_gender)
+    @gender = new_gender
+  end
+
+  def age
+    @age
+  end
+
+  def ethnicity
+    @ethnicity
+  end
+
+  def gender
+    @gender
   end
 
 end
 
-Saint_nick = Santa.new
+
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+santas = []
+
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+p santas
+
+Saint_nick = Santa.new("male", "white")
 Saint_nick.speak
 Saint_nick.eat_milk_and_cookies("Snickerdoodle")
-Saint_nick.intitialize
+p Saint_nick.gender= "unicorn latte female"
+p Saint_nick.get_mad_at("Rudolph")
+p Saint_nick.celebrate_birthday
+p Saint_nick.age
+p Saint_nick.ethnicity
+p Saint_nick.gender

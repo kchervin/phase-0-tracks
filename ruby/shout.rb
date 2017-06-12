543553d7f83dbebdfcf0e8e62718649281306bd2
Kate Chervin <kchervin@gmail.com>
2017-06-12 14:24:23 -0700

    convert a standalone module to a mixin

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,38 @@
+#module Shout
+#  def self.yell_angrily(word)
+#    word + "!!!" + ":("
+#  end
+#  def self.yelling_happily(word)
+#    "!!!" + word + "!!!" + ":)"
+#  end
+#end
+#
+#p Shout.yell_angrily("why??")
+#p Shout.yelling_happily("yay")
+
 module Shout
-  def self.yell_angrily(word)
+  def yell_angrily(word)
     word + "!!!" + ":("
   end
-  def self.yelling_happily(word)
+  def yelling_happily(word)
     "!!!" + word + "!!!" + ":)"
   end
 end
 
-p Shout.yell_angrily("why??")
-p Shout.yelling_happily("yay")
+class Monkey
+  include Shout
+end
+
+class Parrot
+  include Shout
+end
+
+monkey = Monkey.new
+p monkey.yell_angrily("BANANANA")
+p monkey.yelling_happily("taco")
+
+parrot = Parrot.new
+p parrot.yell_angrily("Polly, want a cracker?")
+p parrot.yelling_happily("katie")
+
 

3498803583f9bd636a3f66e05a066d3a60d5ac24
Kate Chervin <kchervin@gmail.com>
2017-06-12 09:46:48 -0700

    complete assignment

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,14 +1,14 @@
-##SPECIES ------------------------
-#puppies
-#
-#CHARACTERISTICS ----------------
-#energy level: high
-#Friendly: yes
-#Eye count: 2
-#Name: varies
-#Color: varies
-#
-#BEHAVIOR -----------------------
-#Jump
-#run
-#fetch
+SPECIES ------------------------
+puppies
+
+CHARACTERISTICS ----------------
+energy level: high
+Friendly: yes
+Eye count: 2
+Name: varies
+Color: varies
+
+BEHAVIOR -----------------------
+Jump
+run
+fetch

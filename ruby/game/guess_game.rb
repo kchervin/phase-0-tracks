6d0836d35b1b63edc92638e56891853a4be51864
Kate Chervin <kchervin@gmail.com>
2017-06-18 22:11:34 -0400

    add pseudocode throughout

--- ruby/game/guess_game.rb
+++ ruby/game/guess_game.rb
@@ -1,6 +1,7 @@
+#create a class for a word guessing game that meets
 class GuessGame
 end
-
+##introduce a first user guess on initilization for a second user to try to guess.
 def initialize(user_1)
   @user_1 = user_1
   @user_1_array = user_1.split('')
@@ -11,12 +12,12 @@ def initialize(user_1)
   @total_guesses = user_1.length
   wrong_letters = []
 end
-
+#create a method for a second user to input letters to guess the user 1 word. code will take sample of letter from alphabet array and output will be a letter string.
 def get_user2
   alphabet = ('a'..'z').to_a
   @user_2 = alphabet.sample
 end
-
+#guesses are limited to the number of letters in the user 1 word.  the incorrect guesses will be stored in an array.  if the user correctly guesses a letter, the count will add one to the count.
 def correct
         if @user_1_array.include? user_2
             correct == true
@@ -24,7 +25,7 @@ def correct
             correct ==  false
         end
 end
-
+#if the user incorrectly guesses a letter that was not guessed before, the count will add one.  if the user guesses a letter that was already guessed, the count will add 0.
 def incorrect
   if !@user_1_array.include? user_2
     correct == false
@@ -32,9 +33,12 @@ def incorrect
   end
 end
 
+#determine number of turns left
 def turns_left
-  @turns_left = @total_guesses - @guess_count
+  turns_left = @total_guesses - @guess_count
 end
+
+#The guessing player receives continual feedback on the current state of the word. So if the secret word is "unicorn", the user will start out seeing something like "_ _ _ _ _ _ _", which would become "_ _ _ c _ _ _" after the user enters a guess of "c".
 
 def show_string(user_1, user_2)
   display_string = ''
@@ -51,7 +55,7 @@ end
 
 play = GuessGame.new("apple")
 
-
+#create parting method reflecting if game was won or lost
 if (@total_guesses == @guess_count) && (@current_dash_appearance  != @user_1)
    @game_over == true
     puts "sorry, you lost."
@@ -59,8 +63,8 @@ if (@total_guesses == @guess_count) && (@current_dash_appearance  != @user_1)
     @game_over == true
     puts "winner!"
   end
-
-  while @game_over == false
+#guesses in the game are limited to the number of letters in the length of the word. if the player guesses the correct letter, count goes up.  if the player guesses an incorrect letter, the count goes up.  if the player guesses a letter that is not correct but has been guessed before, the count does not go up. show turns left and string of correctly guessed letters.
+while @game_over == false
   puts "guess first letter"
    play.get_user2
        if play.correct

fb717ddbd926149b8acfc194638709ac33acf931
Kate Chervin <kchervin@gmail.com>
2017-06-18 22:07:45 -0400

    driver code

--- ruby/game/guess_game.rb
+++ ruby/game/guess_game.rb
@@ -48,3 +48,31 @@ def show_string(user_1, user_2)
   @current_dash_appearance = display_string
 end
 end
+
+play = GuessGame.new("apple")
+
+
+if (@total_guesses == @guess_count) && (@current_dash_appearance  != @user_1)
+   @game_over == true
+    puts "sorry, you lost."
+  elsif @total_guesses != @guess_count  && @current_dash_appearance  == @user_1
+    @game_over == true
+    puts "winner!"
+  end
+
+  while @game_over == false
+  puts "guess first letter"
+   play.get_user2
+       if play.correct
+         puts "The letter was found."
+         guess_count += 1
+       elsif
+         play.incorrect
+         puts "Sorry, that letter is not correct."
+         guess_count += 1
+       elsif
+        !play.correct && !play.incorrect
+       end
+      play.turns_left
+      play.show_string
+end

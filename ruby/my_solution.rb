9bc87d97a898d434f9fc3be2aa216d1ac3a61952
Kate Chervin <kchervin@gmail.com>
2017-06-19 21:51:08 -0400

    complete release 7 '

--- ruby/my_solution.rb
+++ ruby/my_solution.rb
@@ -29,17 +29,26 @@ private
 #predicts death rate based on population density in each state
   def predicted_deaths
     # predicted deaths is solely based on population density
+
+
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      x = 0.4
+      # number_of_deaths = (@population * 0.4).floor
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      x = 0.3
+      # number_of_deaths = (@population * 0.3).floor
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      # number_of_deaths = (@population * 0.2).floor
+      x = 0.2
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      # number_of_deaths = (@population * 0.1).floor
+      x = 0.1
     else
-      number_of_deaths = (@population * 0.05).floor
+      # number_of_deaths = (@population * 0.05).floor
+      x = 0.05
     end
+
+    number_of_deaths = (@population * x).floor
 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
@@ -50,20 +59,21 @@ private
     # by additional factors we haven't added into this functionality.
     speed = 0.0
 
-    if @population_density >= 200
-      speed += 0.5
+    speed += if @population_density >= 200
+       0.5
     elsif @population_density >= 150
-      speed += 1
+      1
     elsif @population_density >= 100
-      speed += 1.5
+      1.5
     elsif @population_density >= 50
-      speed += 2
+      2
     else
-      speed += 2.5
+      2.5
     end
 
     puts " and will spread across the state in #{speed} months.\n\n"
-
+#change to speed += if ...
+#remove "speed += from each individual line "
   end
 
 end

f4a81476e777b616012430b434f0f1fb13d29377
Kate Chervin <kchervin@gmail.com>
2017-06-21 07:37:25 -0400

    add function set up for java

--- js/explore.js
+++ js/explore.js
@@ -3,3 +3,9 @@
  //iterate through each letter of the string - starting with the last index.  for each index that is greater than or equal to zero, loop through and decrement (i--) after each iteration. so starting with reversed_string = "" + the letter at index 0.  Reversed_string is equal to this new letter, and builds as reversed string is equal that letter plus the next changed letter, as reversed_string + string[0] for each letter at ever index.
  // Add driver code that calls the function in order to reverse a string of your choice (as long as it's not a palindrome!), and stores the result of the function in a variable.
 // Add driver code that prints the variable if some condition is true. The condition can be a silly one, like 1 == 1.
+
+function reverse(string) {
+  var reversed_string = "";
+  for (){};
+return
+  }

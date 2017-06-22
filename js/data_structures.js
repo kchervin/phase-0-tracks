5f79dab0e89b3eebe1c5c527d65b94b703a0a28d
Kate Chervin <kchervin@gmail.com>
2017-06-22 16:40:50 -0400

    release 2

--- js/data_structures.js
+++ js/data_structures.js
@@ -13,3 +13,17 @@ for(var i = 0; i < colors.length; i++) {
 }
 
 console.log(horse);
+
+//Horses are great, but times are a-changin', and we need to add an option for faster transportation that we can create with an assembly line. At the bottom of your data_structures.js file, write a constructor function for a car. Give it a few different properties of various data types, including at least one function. Demonstrate that your function works by creating a few cars with it.
+
+function Car(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+
+  this.hum = function() {console.log('hum!'); };
+}
+
+var new_car = new Car('jeep', 'cherokee', 2005);
+console.log(new_car);
+new_car.hum();

88ac9c50cec50a57e66fece70562c179ae3ba248
Kate Chervin <kchervin@gmail.com>
2017-06-22 16:27:58 -0400

    release 1

--- js/data_structures.js
+++ js/data_structures.js
@@ -5,3 +5,11 @@ names.push('evan');
 
 console.log(colors);
 console.log(names);
+
+var horse = {};
+
+for(var i = 0; i < colors.length; i++) {
+  horse[names[i]] = colors[i];
+}
+
+console.log(horse);

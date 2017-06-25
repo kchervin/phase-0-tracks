e34ab149da07893eb4f2e334232ea9904bbd5602
Kate Chervin <kchervin@gmail.com>
2017-06-24 22:09:29 -0400

    create function that takes an integer and driver code that generates and array 10 times

--- js/algos.js
+++ js/algos.js
@@ -1,5 +1,6 @@
 
 //create a variable, set to zero. for each word in the array, identify the length of the string. if the length of the string is greater than 0, the variable will become set to the number of the length of the string.  it will then compare to the word in the following index.  if it is less than the length of the string in that index, its value will become the length of that index.  else, it will remain the same.  until the length of the array is complete. input is an array, output is the longest word in the array.
+
 function longestWord(array) {
   var longest = array[0];
   for (i = 0 ; i < array.length; i++) {
@@ -28,3 +29,49 @@ function compare(object1, object2) {
  }
   return false;
 }
+
+//create a function  that takes an integer (n) and creates an array of (n) words.  so while i < n, create new strings randomly selected from the alphabet and push them into an array.
+//to create new string, counter is set to zero,
+ // while counter is less than randomly selected number, count up from zero to create the length of the new string.
+ //Randomly select letters from the alphabet by randomly selecting number of index of letter.
+ //store strings of random lengths and random letters in new array.
+ //return the new array.
+ //input willl be an integer, output is an array.
+
+function createNew(n) {
+  var newWords = [];
+  for (var i = 0; i < n; i++) {
+    var alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    var newString = '';
+
+    for (var y = 0; y < Math.random() * 10; y++) {
+      newString += alphabet.charAt(Math.floor(Math.random( ) * 26));
+    }
+  newWords.push(newString);
+}
+return newWords;
+}
+
+
+
+//driver code
+
+var gerbals = ['samantha', 'sam', 'sammy', 'samson']
+var puppies = ['ted', 'teddy', 'theodore'];
+var kittens = ["Buster", "Princess", "Minxy"];
+console.log(longestWord (kittens));
+console.log(longestWord (puppies));
+console.log(longestWord (gerbals));
+
+console.log(compare({name: "Steven", age: 54}, {name: "Tamir", age: 54}));
+console.log(compare({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3}));
+console.log(compare({animal: "Dog", legs: 4}, {animal: "Cat", legs: 3}));
+console.log(createNew(3));
+console.log(createNew(4));
+console.log(createNew(5));
+
+for (var x=0; x<10; x++) {
+console.log(longestWord(createNew(3)));
+}
+
+//set counter to 0.  ten times - create new arrays of length determined by parameter.  in new array, store words of vary lengths and letters. then, for each array, determine the longest word and print. input will be a number, output will be the longest word of an array.

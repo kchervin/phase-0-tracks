112fbfe10ee4ac1fee26d8f64de1f51ba9660c5a
Kate Chervin <kchervin@gmail.com>
2017-07-02 20:41:45 -0400

    Create new database and ruby file

---
+++ databases/solo.rb
@@ -0,0 +1,17 @@
+require 'sqlite3'
+
+
+db = SQLite3::Database.new("solo.db")
+
+
+# So far in this course, you've had to satisfy a rubric by reading for detail, asking questions to resolve ambiguity, and building programs to spec. These are crucial skills for a developer.
+
+# But learning and self-development skills are important, too:
+
+# What do you do with your free time?
+# How do you find ways to push yourself when there's no one around to tell you what to do?
+# How do you find new ideas to explore when we aren't providing them as bullets?
+# Your answers to these questions will have a huge impact on your success, and today is your first official opportunity to practice.
+
+# DO THE THING
+# Write a Ruby program that uses persistent data to make your life better, or makes someone else's life better. Find several opportunities to try something not explicitly shown in the video. Can users create data? Retrieve it? Manipulate it? It's up to you. Push yourself, and decide with integrity when you are finished. Note that you do not need to use classes in this assignment -- keeping a class instance and a database row in sync is a lot of work, so that might be too much to tackle.

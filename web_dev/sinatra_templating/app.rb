1b5b40af10c93c111efd51ab4f4982af1f37e5f8
Kate Chervin <kchervin@gmail.com>
2017-07-12 17:57:02 -0700

    Create final assignment"

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -17,11 +17,22 @@ get '/students/new' do
   erb :new_student
 end
 
+get'/campus_list' do
+  @students = db.execute("SELECT * FROM students")
+  @campuses = db.execute("SELECT city FROM campuses")
+  erb :campus_list
+end
+
 # create new students via
 # a form
 post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
+end
+
+post '/campuses' do
+  db.execute("INSERT INTO campuses (city) VALUES (?)", [params['campus']])
+  redirect 'campus_list'
 end
 
 # add static resources
